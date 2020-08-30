a3882dfe7bd95f4be6e03a17cf14ac0e72c5b45a
Kevin Canlas <kvncnlsdsgns@gmail.com>
2020-08-30 17:41:13 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+replaceText(document.body);
+
+function replaceText(element) {
+  if (element.hasChildNodes()) {
+    element.childNodes.forEach(replaceText);
+  } else if (element.nodeType === Text.TEXT_NODE) {
+    const noCorona = document.createElement("span");
+    noCorona.innerHTML = element.textContent.replace(
+      /(coronavirus)/gi,
+      '<span class="blackedOut">$1</span>'
+    );
+
+    const noCovid = document.createElement("span");
+    noCovid.innerHTML = element.textContent.replace(
+      /(covid\-19)/gi,
+      '<span class="blackedOut">$1</span>'
+    );
+
+    if (element.textContent.match(/coronavirus/gi)) {
+      element.replaceWith(noCorona);
+    }
+    if (element.textContent.match(/covid\-19/gi)) {
+      element.replaceWith(noCovid);
+    }
+  }
+}
